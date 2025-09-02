d30f0f046b8c8c39d21a47c350173f59137b7de5
Akseli Kettunen <akseli.kettunen@helsinki.fi>
2025-09-02 14:27:30 +0300

    ex 10.11. repos now fetched with Apollo client.

--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -1,30 +1,20 @@
-import { useEffect, useState } from "react";
+import { useQuery } from "@apollo/client";
+import { GET_REPOSITORIES } from "../graphql/queries";
 
 const useRepositories = () => {
-  const [repositories, setRepositories] = useState();
-  const [loading, setLoading] = useState(false);
+  const { data, refetch, loading, error } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: "cache-first",
+  });
 
-  const fetchRepositories = async () => {
-    setLoading(true)
+  // eslint-disable-next-line no-undef
+  console.log("data in UR: ", data);
 
-    // eslint-disable-next-line no-undef
-    const response = await fetch("http://172.17.212.37:5000/api/repositories");
-    const json = await response.json();
-
-
-    setLoading(false)
-
-    // eslint-disable-next-line no-undef
-    console.log(json);
-
-    setRepositories(json);
+  return {
+    repositories: data ? data.repositories : false,
+    loading,
+    refetch,
+    error,
   };
+};
 
-  useEffect(() => {
-    fetchRepositories();
-  }, []);
-
-  return { repositories, loading, refetch: fetchRepositories }
-}
-
-export default useRepositories
+export default useRepositories;
