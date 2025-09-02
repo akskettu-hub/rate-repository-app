fd465af3eaf5689f3b2e0bca006c30c5ee0c04e6
Akseli Kettunen <akseli.kettunen@helsinki.fi>
2025-09-02 09:24:28 +0300

    Ex 10.8 Add log in form.

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,7 +1,66 @@
-import Text from './Text';
+import { Pressable, StyleSheet, TextInput, View } from "react-native";
+import Text from "./Text";
+import { useFormik } from "formik";
+import theme from "../theme";
+
+const initialValues = {
+  username: "",
+  password: "",
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "column",
+  },
+  input: {
+    margin: 12,
+    borderWidth: 2,
+    borderRadius: 6,
+    borderColor: theme.colors.textSecondary,
+    padding: 10,
+  },
+  submit: {
+    backgroundColor: theme.colors.primary,
+    borderColor: theme.colors.primary,
+    margin: 12,
+    borderRadius: 6,
+    padding: 10,
+    alignItems: "center",
+  },
+});
+
+const onSubmit = (values) => {
+  console.log(values);
+};
 
 const SignIn = () => {
-  return <Text>The sign-in view</Text>;
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+  });
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        placeholder="Username"
+        value={formik.values.username}
+        style={styles.input}
+        onChangeText={formik.handleChange("username")}
+      />
+      <TextInput
+        placeholder="Password"
+        value={formik.values.password}
+        style={styles.input}
+        onChangeText={formik.handleChange("password")}
+        secureTextEntry={true}
+      />
+      <Pressable onPress={formik.handleSubmit} style={styles.submit}>
+        <Text fontSize={"subheading"} fontWeight={"bold"} color={"white"}>
+          Sign In
+        </Text>
+      </Pressable>
+    </View>
+  );
 };
 
 export default SignIn;
